dd010de0fc005239a176683402f6939e7978d3eb
Alexandr <a.a.shapovov@bk.ru>
2024-02-26 13:37:58 +0300

    test

---
+++ AlgoritmsAndDataStructures/ES/Time_complexity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsAndDataStructures.ES
+{
+    public class Time_complexity
+    {
+        public static IEnumerable<int> ReadInts(string filePath)
+        {
+            using (TextReader reader = File.OpenText(filePath))
+            {
+                string lastLine;
+                while ((lastLine = reader.ReadLine()) != null)
+                {
+                    yield return int.Parse(lastLine);
+                }
+
+            }
+        }
+    }
+
+    public class ThreeSum
+    {
+        public static int Count(int[] a)
+        {
+            int n = a.Length;
+            int counter = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    for (int k = j + 1; k < n; k++)
+                    {
+                        if (a[i] + a[j] + a[k] == 0)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+
+            return counter;
+        }
+    }
+
+
+}
--- AlgoritmsAndDataStructures/Program.cs
+++ AlgoritmsAndDataStructures/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AlgoritmsAndDataStructures.ES;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,15 @@ namespace AlgoritmsAndDataStructures
     {
         static void Main(string[] args)
         {
-            int a = new int();
+            var ints = Time_complexity.ReadInts("1Kints.txt").ToArray();
+            var wathc = new Stopwatch();
+            wathc.Start();
+
+            var triplets = ThreeSum.Count(ints);
+            wathc.Stop();
+
+            Console.WriteLine($"The number of \"zero-sum\" triplets :{triplets}");
+            Console.WriteLine($"The taken {wathc.Elapsed:g}");
         }
     }
 }
